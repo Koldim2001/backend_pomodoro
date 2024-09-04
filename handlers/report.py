e9861816b6372f83925eb71389966ef1d29159f0
Колесников Дмитрий Андреевич <kolesnikovda@digtp.com>
2024-09-04 14:23:51 +0300

    report tag

--- handlers/report.py
+++ handlers/report.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from celery_workers.email import send_email_report_tasks
 from utils.jwt import get_request_user_id
 
-router = APIRouter(prefix="/report")
+router = APIRouter(prefix="/report", tags=["report"])
 
 
 @router.post(path="/all_tasks",summary="Отправление на почту списка всех задач",
