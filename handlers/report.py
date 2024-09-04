ed3da835335fe148cb1f95131a1fe002aa77f114
Колесников Дмитрий Андреевич <kolesnikovda@digtp.com>
2024-09-04 10:18:41 +0300

    email report router

---
+++ handlers/report.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+from utils.email import send_email_report_tasks
+from utils.jwt import get_request_user_id
+
+router = APIRouter(prefix="/report")
+
+
+@router.post(path="/all_tasks",summary="Отправление на почту списка всех задач",
+    description="Отправляет на почту список всех имеющихся задач пользователя",
+)
+def get_dashboard_report(recipient_email: str, user_id: int = Depends(get_request_user_id)):
+    send_email_report_tasks.delay(recipient_email, user_id)
+    return {
+        "status": 200,
+        "data": "Письмо отправлено",
+        "recipient_email": recipient_email
+    }
